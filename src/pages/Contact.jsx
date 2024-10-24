13a6a32cea22fd3191f5324360e11cea6e7385fa
Harshit shukla <harshitshuklaharsh8@gmail.com>
2024-10-24 17:01:10 +0530

    contact us redesigned

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,35 +1,61 @@
-import { useEffect, useRef, useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
-  Mail,
-  Phone,
-  Globe,
-  Facebook,
-  Instagram,
-  Twitter,
-  Linkedin,
   ArrowRight,
 } from "lucide-react";
 import { motion } from "framer-motion";
 
 export default function Contact() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission logic here
-    console.log("Form submitted");
-  };
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+    phone: "",
+  });
 
+  const [formStatus, setFormStatus] = useState(""); // State to show form submission status
   const [isVisible, setIsVisible] = useState(false); // State to track visibility
   const contactRef = useRef(null); // Ref for the Contact section
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:5000/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        setFormStatus("Form submitted successfully!");
+        setFormData({
+          name: "",
+          email: "",
+          subject: "",
+          message: "",
+          phone: "",
+        }); // Reset form fields
+      } else {
+        setFormStatus("Error submitting the form. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setFormStatus("Error submitting the form. Please try again.");
+    }
+  };
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          setIsVisible(true); // Set visibility to true when in view
-          observer.disconnect(); // Stop observing after it becomes visible
+          setIsVisible(true);
+          observer.disconnect();
         }
       },
-      { threshold: 0.1 } // Trigger when 10% of the element is in view
+      { threshold: 0.1 }
     );
 
     if (contactRef.current) {
@@ -44,67 +70,37 @@ export default function Contact() {
   }, []);
 
   return (
-    <div className="max-w-6xl mx-auto p-6" ref={contactRef}>
-      <motion.h1
-        className="text-5xl mb-8 font-cormorant"
-        initial={{ opacity: 0, y: -20 }} // Initial animation settings
-        animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }} // Animate based on visibility
-        transition={{ duration: 0.5 }} // Animation duration
-      >
-        Get in touch
-      </motion.h1>
-      <div className="flex flex-col md:flex-row gap-8">
+    <div
+      className="relative flex h-screen"
+      ref={contactRef}
+      style={{
+        backgroundImage: "url('/public/assets/images/livingroom1.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      {/* Left Side (Black Background) */}
+
+      {/* Right Side (Form Section) */}
+      <div className="relative w-full flex items-center justify-center bg-black bg-opacity-50 ">
+        <div className="w-[50vw]"></div>
         <motion.div
-          className="md:w-1/3 bg-secondaryBlack p-8 rounded-lg flex flex-col justify-evenly space-y-6 md:space-y-0"
-          initial={{ opacity: 0, x: -50 }} // Initial animation settings
-          animate={isVisible ? { opacity: 1, x: 0 } : { opacity: 0, x: -50 }} // Animate based on visibility
-          transition={{ duration: 0.5 }} // Animation duration
+          className="w-full md:w-2/3 lg:w-1/2 px-6 py-8 pt-32 bg-black h-full bg-opacity-80"
+          initial={{ opacity: 0, y: 50 }}
+          animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+          transition={{ duration: 0.5 }}
         >
-          <div className="flex items-center space-x-3">
-            <Mail className="text-primaryRed" />
-            <span>info@yourdomain.com</span>
-          </div>
-          <div className="flex items-center space-x-3">
-            <Phone className="text-primaryRed" />
-            <span>+1 (378) 400-1234</span>
-          </div>
-          <div className="flex items-center space-x-3">
-            <Globe className="text-primaryRed" />
-            <span>www.yourdomain.com</span>
-          </div>
-          <div className="mt-8 flex space-x-10">
-            <a
-              href="#"
-              className="text-primaryRed hover:text-white transition-colors duration-300"
-            >
-              <Facebook size={20} />
-            </a>
-            <a
-              href="#"
-              className="text-primaryRed hover:text-white transition-colors duration-300"
-            >
-              <Instagram size={20} />
-            </a>
-            <a
-              href="#"
-              className="text-primaryRed hover:text-white transition-colors duration-300"
-            >
-              <Twitter size={20} />
-            </a>
-            <a
-              href="#"
-              className="text-primaryRed hover:text-white transition-colors duration-300"
-            >
-              <Linkedin size={20} />
-            </a>
-          </div>
-        </motion.div>
-        <motion.div
-          className="md:w-2/3"
-          initial={{ opacity: 0, x: 50 }} // Initial animation settings
-          animate={isVisible ? { opacity: 1, x: 0 } : { opacity: 0, x: 50 }} // Animate based on visibility
-          transition={{ duration: 0.5 }} // Animation duration
-        >
+          <motion.h1
+            className="text-4xl lg:text-5xl mb-8 font-cormorant text-white text-center"
+            initial={{ opacity: 0, y: -20 }}
+            animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
+          >
+            Get in touch
+          </motion.h1>
+        
+
+          {/* Form */}
           <form onSubmit={handleSubmit} className="space-y-4">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div>
@@ -118,8 +114,12 @@ export default function Contact() {
                   type="text"
                   id="name"
                   name="name"
+                  value={formData.name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
                   required
-                  className="w-full px-3 py-2 bg-secondaryBlack border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full px-3 py-2 bg-secondaryBlack bg-opacity-50 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-white"
                 />
               </div>
               <div>
@@ -133,11 +133,16 @@ export default function Contact() {
                   type="email"
                   id="email"
                   name="email"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
                   required
-                  className="w-full px-3 py-2 bg-secondaryBlack border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full px-3 py-2 bg-secondaryBlack bg-opacity-50 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-white"
                 />
               </div>
             </div>
+
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div>
                 <label
@@ -150,8 +155,12 @@ export default function Contact() {
                   type="text"
                   id="subject"
                   name="subject"
+                  value={formData.subject}
+                  onChange={(e) =>
+                    setFormData({ ...formData, subject: e.target.value })
+                  }
                   required
-                  className="w-full px-3 py-2 bg-secondaryBlack border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full px-3 py-2 bg-secondaryBlack bg-opacity-50 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-white"
                 />
               </div>
               <div>
@@ -165,10 +174,15 @@ export default function Contact() {
                   type="tel"
                   id="phone"
                   name="phone"
-                  className="w-full px-3 py-2 bg-secondaryBlack border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  value={formData.phone}
+                  onChange={(e) =>
+                    setFormData({ ...formData, phone: e.target.value })
+                  }
+                  className="w-full px-3 py-2 bg-secondaryBlack bg-opacity-50 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-white"
                 />
               </div>
             </div>
+
             <div>
               <label
                 htmlFor="message"
@@ -179,12 +193,17 @@ export default function Contact() {
               <textarea
                 id="message"
                 name="message"
+                value={formData.message}
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
                 rows={6}
                 required
-                className="w-full px-3 py-2 bg-secondaryBlack border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full px-3 py-2 bg-secondaryBlack bg-opacity-50 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-white"
                 placeholder="Hello, I am interested in.."
               ></textarea>
             </div>
+
             <div className="flex justify-end">
               <button
                 type="submit"
@@ -195,6 +214,11 @@ export default function Contact() {
               </button>
             </div>
           </form>
+
+          {/* Form submission status */}
+          {formStatus && (
+            <p className="mt-4 text-center text-primaryRed">{formStatus}</p>
+          )}
         </motion.div>
       </div>
     </div>
